28fa5e823772ee99ffee26eb6dd4d2fbf04947df
koki-develop <kou.pg.0131@gmail.com>
2024-07-12 20:01:42 +0900

    Add function_param resource

---
+++ internal/provider/resource_function_param.go
@@ -0,0 +1,81 @@
+package provider
+
+import (
+	"context"
+
+	"github.com/hashicorp/terraform-plugin-framework/resource"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
+	"github.com/hashicorp/terraform-plugin-framework/types"
+)
+
+var (
+	_ resource.Resource = &resourceFunctionParam{}
+)
+
+func NewResourceFunctionParam() resource.Resource {
+	return &resourceFunctionParam{}
+}
+
+type resourceFunctionParam struct{}
+
+func (r *resourceFunctionParam) Metadata(_ context.Context, req resource.MetadataRequest, resp *resource.MetadataResponse) {
+	resp.TypeName = req.ProviderTypeName + "_function_param"
+}
+
+func (r *resourceFunctionParam) Schema(_ context.Context, _ resource.SchemaRequest, resp *resource.SchemaResponse) {
+	resp.Schema = schema.Schema{
+		Attributes: map[string]schema.Attribute{
+			"name": schema.StringAttribute{
+				Required: true,
+			},
+
+			"id": schema.StringAttribute{
+				CustomType: ID{},
+				Computed:   true,
+			},
+		},
+	}
+}
+
+type resourceFunctionParamModel struct {
+	Name types.String `tfsdk:"name"`
+	ID   IDValue      `tfsdk:"id"`
+}
+
+func (r *resourceFunctionParam) Create(ctx context.Context, req resource.CreateRequest, resp *resource.CreateResponse) {
+	var plan resourceFunctionParamModel
+	diags := req.Plan.Get(ctx, &plan)
+	resp.Diagnostics.Append(diags...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	plan.ID = NewIDValue(plan.Name)
+	diags = resp.State.Set(ctx, &plan)
+	resp.Diagnostics.Append(diags...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+}
+
+func (r *resourceFunctionParam) Read(ctx context.Context, req resource.ReadRequest, resp *resource.ReadResponse) {
+}
+
+func (r *resourceFunctionParam) Update(ctx context.Context, req resource.UpdateRequest, resp *resource.UpdateResponse) {
+	var plan resourceFunctionParamModel
+	diags := req.Plan.Get(ctx, &plan)
+	resp.Diagnostics.Append(diags...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	plan.ID = NewIDValue(plan.Name)
+	diags = resp.State.Set(ctx, &plan)
+	resp.Diagnostics.Append(diags...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+}
+
+func (r *resourceFunctionParam) Delete(ctx context.Context, req resource.DeleteRequest, resp *resource.DeleteResponse) {
+}
