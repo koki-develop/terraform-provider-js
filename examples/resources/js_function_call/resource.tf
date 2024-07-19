256b86c3324dbd0c9d279c95c3070fe28fd2a797
koki-develop <kou.pg.0131@gmail.com>
2024-07-20 08:28:59 +0900

    Add example

--- examples/resources/js_function_call/resource.tf
+++ examples/resources/js_function_call/resource.tf
@@ -1,3 +1,8 @@
+resource "js_function_call" "alert" {
+  function = "alert"
+  args     = ["hello world"]
+}
+
 resource "js_function_call" "console_log" {
   caller   = "console"
   function = "log"
